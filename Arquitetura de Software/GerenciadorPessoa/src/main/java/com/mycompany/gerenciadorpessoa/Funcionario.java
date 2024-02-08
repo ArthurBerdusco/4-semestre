912864c55b606178a978875031a8365999e61450
ARTHUR SILVA BERDUSCO DE SOUZA <arthur.sbsouza@senacsp.edu.br>
2024-02-07 22:13:48 -0300

    finalizado aula 02 arquitetura de software

--- Arquitetura de Software/GerenciadorPessoa/src/main/java/com/mycompany/gerenciadorpessoa/Funcionario.java
+++ Arquitetura de Software/GerenciadorPessoa/src/main/java/com/mycompany/gerenciadorpessoa/Funcionario.java
@@ -1,15 +1,19 @@
 package com.mycompany.gerenciadorpessoa;
 
-public class Funcionario extends PessoaFisica{
+public class Funcionario extends PessoaFisica {
+
     private int matricula;
-    
+    private String cargo;
+    private double salario;
 
     public Funcionario() {
     }
 
-    public Funcionario(String nome, long cpf, int matricula) {
+    public Funcionario(String nome, long cpf, int matricula, String cargo, double salario) {
         super(nome, cpf);
         this.matricula = matricula;
+        this.cargo = cargo;
+        this.salario = salario;
     }
 
     public int getMatricula() {
@@ -25,8 +29,30 @@ public class Funcionario extends PessoaFisica{
         return "Funcionario{" + "nome=" + getNome() + ", cpf=" + getCpf() + ", matricula=" + matricula + '}';
     }
 
-    
-    
-    
-    
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String alterarSalario(Funcionario funcionario, double novoSalario) {
+        if (this.cargo.equals("Gerente") && this.matricula != funcionario.getMatricula()) {
+            funcionario.setSalario(novoSalario);
+            return "Sucesso - Salário do funcionario alterado com sucesso";
+        } else if (this.cargo.equals("Gerente") && this.matricula == funcionario.getMatricula()) {
+            return "Falha - O gerente não pode alterar o proprio salário";
+        } 
+        return "Falha - Este cargo não tem permissão para alterar o salária";
+    }
+
 }
--- Arquitetura de Software/GerenciadorPessoa/src/main/java/com/mycompany/gerenciadorpessoa/Main.java
+++ Arquitetura de Software/GerenciadorPessoa/src/main/java/com/mycompany/gerenciadorpessoa/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         var pJuridica = new PessoaJuridica("Arthur", 0046065455546L);
         var pFisica = new PessoaFisica("João", 42819319882L);
-        var pFuncionario = new Funcionario("Carlos", 42819319882L , 11340);
+        var gerente = new Funcionario("Carlos", 42819319882L , 11340, "Gerente", 9507);
+        var engenharia = new Funcionario("Maria", 42819319882L , 24056, "Engenharia", 4300);
         
-        System.out.println(pFuncionario);
-        
-        
+        System.out.println(gerente.alterarSalario(engenharia, 5000));
+        System.out.println(gerente.alterarSalario(gerente, 5000));
+        System.out.println(engenharia.alterarSalario(gerente, 3500));
+
     }
 }
