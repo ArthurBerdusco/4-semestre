25e1b77c131d5321b83127b78257af4b056ddfb3
Arthur <arthur.berdusco1@gmail.com>
2024-02-08 23:26:03 -0300

    finalizado licao de casa sistemas web

---
+++ Desenvolvimento de Sistemas Web/05-02-2024/licao de casa/exercicio-aula01/src/main/java/br/senac/tads/dsw/exercicioaula01/FormacaoAcademica.java
@@ -0,0 +1,17 @@
+package br.senac.tads.dsw.exercicioaula01;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FormacaoAcademica {
+    private String nomeInstituto;
+    private String curso;
+    public FormacaoAcademica(String nomeInstituto, String curso) {
+        this.nomeInstituto = nomeInstituto;
+        this.curso = curso;
+    }
+
+    
+}
