8b0511d8e7fb0e4ea824b0575a18982d5d6b8596
ARTHUR SILVA BERDUSCO DE SOUZA <arthur.sbsouza@senacsp.edu.br>
2024-03-25 20:51:15 -0300

    ado

---
+++ Arquitetura de Software/ADO1/Exercicio1/src/main/java/caixa/Exercicio1.java
@@ -0,0 +1,12 @@
+package caixa;
+
+import loja.Estoque;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Estoque estoque = new Estoque();
+        
+        
+    }
+}
